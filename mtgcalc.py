75279836f5b418b8577f4e67306956f7538d5b9f
manue1perry <kojosey1994@gmail.com>
2023-01-19 15:51:17 -0500

    debugging if/else statement

--- mtgcalc.py
+++ mtgcalc.py
@@ -1,10 +1,11 @@
 def calculate(principal,anual_rate,loan_term):
     no_of_payments = loan_term * 12
     monthly_rate = anual_rate / 12
-    if anual_rate is 0:
+    if anual_rate == 0:
         monthly_payment = principal/no_of_payments
     else:
         monthly_payment = (principal * monthly_rate)/(1-(1+monthly_rate)**-1)
+
     return monthly_payment
 
 
